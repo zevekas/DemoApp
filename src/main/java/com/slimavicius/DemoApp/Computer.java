a48ec30f2dfcb25376fccaafb21faf38bcb3280b
Vytas Klimavičius <vytas@Vytass-Laptop.local>
2025-01-26 22:28:10 +0200

    7th tutorial done

---
+++ src/main/java/com/slimavicius/DemoApp/Computer.java
@@ -0,0 +1,5 @@
+package com.slimavicius.DemoApp;
+
+public interface Computer {
+    void compile();
+}
--- src/main/java/com/slimavicius/DemoApp/Dev.java
+++ src/main/java/com/slimavicius/DemoApp/Dev.java
@@ -1,15 +1,20 @@
 package com.slimavicius.DemoApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
 
-    Laptop laptop;
+    private Computer comp;
 
+    @Autowired
+    public void setLaptop(Laptop laptop) {
+        this.comp = laptop;
+    }
     public void build() {
 
-        laptop.compile();
+        comp.compile();
         System.out.println("Working on an Awesome project");
     }
 }
